875d83f03166ad9dcf023be4adce8ec6c3e49456
vshantam <vshantam@yahoo.com>
2017-04-22 05:12:55 +0530

    solved encoding error

--- Mian.py
+++ Mian.py
@@ -2,13 +2,12 @@ import sys
 import urllib.request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-url=#Post your url "
+url="https://www.google.co.in/"
 try:
   get_url=urlopen(url)#getting responses from the server.
   get_code=get_url.read()#Reads the Entire html code.
   soup=BeautifulSoup(get_code,'lxml')#parsing the web content.
-  print(soup.find_all('html')[0])#Displays the entire html code and removes the unnecessary tabs, allingment,tags etc.
-except HTTPError:
-  print("Server error\nTry again later\n")
-except Excetion as e:
-  print(str(e))
+  print((soup.find_all('html')[0]).encode('utf-8'))#Displays the entire html code and removes the unnecessary tabs, allingment,tags etc.
+except Exception as e:
+  print("\nTry again later\n")
+  print('Error code: ', str(e))
