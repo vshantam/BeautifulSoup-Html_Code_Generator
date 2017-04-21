c811374b21ba68796d2422496dcc9b812de9420f
vshantam <vshantam@yahoo.com>
2017-04-22 04:46:46 +0530

    Added Exceptions

--- Mian.py
+++ Mian.py
@@ -3,7 +3,12 @@ import urllib.request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 url=#Post your url "
-get_url=urlopen(url)
-get_code=get_url.read()
-soup=BeautifulSoup(get_code,'lxml')
-print(soup.find_all('html')[0])
+try:
+  get_url=urlopen(url)
+  get_code=get_url.read()
+  soup=BeautifulSoup(get_code,'lxml')
+  print(soup.find_all('html')[0])
+except HTTPError:
+  print("Server error\nTry again later\n")
+except Excetion as e:
+  print(str(e))
