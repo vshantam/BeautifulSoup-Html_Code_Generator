a2df1a1a6583923a7881f995208375e33fdf31a0
vshantam <vshantam@yahoo.com>
2017-04-22 04:50:58 +0530

    adding comments

--- Mian.py
+++ Mian.py
@@ -4,10 +4,10 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 url=#Post your url "
 try:
-  get_url=urlopen(url)
-  get_code=get_url.read()
-  soup=BeautifulSoup(get_code,'lxml')
-  print(soup.find_all('html')[0])
+  get_url=urlopen(url)#getting responses from the server.
+  get_code=get_url.read()#Reads the Entire html code.
+  soup=BeautifulSoup(get_code,'lxml')#parsing the web content.
+  print(soup.find_all('html')[0])#Displays the entire html code and removes the unnecessary tabs, allingment,tags etc.
 except HTTPError:
   print("Server error\nTry again later\n")
 except Excetion as e:
