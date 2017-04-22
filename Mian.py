13f28a546b430e4e57dd88f8d757276357f24954
vshantam <vshantam@yahoo.com>
2017-04-22 17:26:48 +0530

    Minor Bug fixed

--- Mian.py
+++ Mian.py
@@ -18,11 +18,12 @@ def main():
                 soup=soup.find('html').encode('utf-8')#taking care for encoding error
                 print("The HTML code :\n\n\n")
                 print(soup)#Displaying the entire HTML code
+                soup=BeautifulSoup(get_code,'lxml')
                 link_count=len(soup.find_all("a"))#counting the number of hyertext link
                 print("The Hyper link present in the page are {}".format(link_count))
                 for i in range(len(soup.find_all("a"))):
                     print("For {}::The url is ::{}".format(soup.find_all("a")[i].text,soup.find_all("a")[i].get('href')))#Displaying the hyper text links with their links
-                choice=input("Do you Wish to continue?(Yes/No").strip()
+                choice=input("Do you Wish to continue?(Yes/No)".strip()
                 choice=choice.lower()[0]
         except KeyboardInterrupt:
                 print("\n{0}Exiting Script\n{1}Interrupted by the user\n".format('\33[93m','\.33[1;35m'))#Taking care of Exceptions
